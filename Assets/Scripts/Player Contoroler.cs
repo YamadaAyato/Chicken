bc556b2636650dbb8d3c76ad0889c4dd7a34ccad
ayato0913a <ayato0913a@gmail.com>
2025-06-09 03:35:50 +0900

    [add]Player Contorolerを追加 　　　卵のプレハブを追加 　　　Chickenの画像を追加

---
+++ Assets/Scripts/Player Contoroler.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoler : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 10f;
+    [SerializeField] private float _jumpforce = 10f;
+    private Rigidbody2D _rb;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        if (_rb == null)
+        {
+            Debug.LogError("Rigidbody2D がアタッチされていません！");
+        }
+    }
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+    private void Move()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+        if (_rb != null)
+        {
+            _rb.AddForce(Vector2.right * move * _moveSpeed);
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && _rb != null)
+        {
+            _rb.AddForce(Vector2.up * _jumpforce, ForceMode2D.Impulse);
+        }
+    }
+
+}
