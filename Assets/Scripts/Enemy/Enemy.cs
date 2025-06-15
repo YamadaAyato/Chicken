c8e5846964cad45ba329a552f9d208828926e62d
ayato0913a <ayato0913a@gmail.com>
2025-06-15 14:41:12 +0900

    [add]ジャンプ移動する敵の処理をEnemyスクリプトに作成 　　横移動の敵の動きを改善

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -9,15 +9,24 @@ public class Enemy : MonoBehaviour
     private int _currentHp;
 
     [Header("速度設定")]
-    [SerializeField] private float _moveForce = 10f;　//移動速度
+    [SerializeField] private float _baseSpeed = 10f;　//移動速度
     [SerializeField] private float _maxSpeed = 50f;　//速度制限
-    private float _baseMoveForce;
+    private float _currentSpeed;
+
+    [Header("ジャンプ設定")]
+    [SerializeField] private float _jumpForce = 10f;             // ジャンプ力
+    [SerializeField] private float _jumpHorizontalSpeed = 5f;    // 左方向へのジャンプ速度
 
     [Header("鈍足設定")]
     [SerializeField] private float _slowMultiplier = 0.5f;  // 速度を何倍にするか
     [SerializeField] private float _slowDuration = 1f;      // 鈍足時間（秒）
 
-    private bool _isSlowed = false;
+    [Header("敵別設定")]
+    [SerializeField] private bool _move;　//横移動
+    [SerializeField] private bool _jump;　//ジャンプ移動
+
+    private bool _isSlowed = false;　　//スロー状態の判定
+    private bool _isGrounded = false;　//接地判定
 
     Rigidbody2D _rb;
 
@@ -25,27 +34,37 @@ public class Enemy : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody2D>();
         _currentHp = _maxHp;
-        _baseMoveForce = _moveForce;
+        _currentSpeed = _baseSpeed;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        EnemyMove();
+        if (_move)
+        {
+            EnemyMove();
+        }
+
+        if (_jump && _isGrounded) 
+        {
+            EnemyJump();
+        }
     }
 
     private void EnemyMove()
     {
-        if (_rb.velocity.x > -_maxSpeed)
-        {
-            _rb.AddForce(Vector2.left * _moveForce);
-        }
+        // 左方向に一定速度で移動（速度制限あり）
+        //Mathf.Clamp(制限したい値, 最小値, 最大値)
+        float clampedSpeed = Mathf.Clamp(-_currentSpeed, -_maxSpeed, _maxSpeed);
+        //_rb.velocity.y で y方向の速度（ジャンプ・落下など）を保持
+        _rb.velocity = new Vector2(clampedSpeed, _rb.velocity.y);
+    }
 
-        //最大速度を制限
-        if (_rb.velocity.x < -_maxSpeed)
-        {
-            _rb.velocity = new Vector2(-_maxSpeed, _rb.velocity.y);
-        }
+    private void EnemyJump()
+    {
+        // 左斜め上にジャンプ
+        _rb.velocity = new Vector2(-_jumpHorizontalSpeed, _jumpForce);
+        _isGrounded = false; // 空中にいる状態に戻す
     }
 
     public void TakeDamage(int damage)
@@ -73,12 +92,20 @@ public class Enemy : MonoBehaviour
     {
         _isSlowed = true;
         //移動速度に遅くする倍率をかける
-        _moveForce = _baseMoveForce * _slowMultiplier;
-
+        _currentSpeed = _baseSpeed * _slowMultiplier;
+        //時間経過後、下の処理を実行
         yield return new WaitForSeconds(_slowDuration);
         //移動速度を戻す
-        _moveForce = _baseMoveForce;
+        _currentSpeed = _baseSpeed; ;
         _isSlowed = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = true;
+        }
     }
 }
 
